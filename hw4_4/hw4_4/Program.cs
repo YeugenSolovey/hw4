d1937d81b8442f9e680834f9872f2241f2104c91
YeugenSolovey <116490331+YeugenSolovey@users.noreply.github.com>
2022-11-23 22:46:57 +0200

    Add files via upload

--- hw4_4/hw4_4/Program.cs
+++ hw4_4/hw4_4/Program.cs
@@ -34,7 +34,7 @@ abstract class GeograficObj
         _highestPoint = highestPoint;
 
         res = "x coord: " + xCoord.ToString() + " - y cood: " + yCoord.ToString() + " - name: " +
-            name + " - description: " + description + " - highest point: " + highestPoint.ToString();
+            name + " - description: " + description + " - highest point: " + highestPoint.ToString() + "\n";
     }
    
     public void ShowInfo()
@@ -66,48 +66,94 @@ internal class Program
         int spead;
         int riverLong;
         int highestPoint;
-        try
+        River r = new River();
+        Mountain m = new Mountain();
+        while (true)
         {
-            Console.WriteLine("Enter river params: \n");
+            try
+            {
+                Console.WriteLine("Enter river params: ");
 
-            Console.WriteLine("Enter x coord: ");
-            xCoord = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter y coord: ");
-            yCoord = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter name: ");
-            name = Console.ReadLine();
-            Console.WriteLine("Enter description: ");
-            description = Console.ReadLine();
-            Console.WriteLine("Enter speed: ");
-            spead = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter river long: ");
-            riverLong = Convert.ToInt32(Console.ReadLine());
 
-            River r = new River();
+                Console.WriteLine("Enter name: ");
+                name = Console.ReadLine();
+                bool flag = char.IsDigit(name[0]);
+                if (flag)
+                {
+                    Console.WriteLine("Invalid name, try later after mountain");
+                }
+                else if (name[0] == '+')
+                {
+                    Console.WriteLine("Invalid name, try later after mountain");
+                }
+                else if (name[0] == '-')
+                {
+                    Console.WriteLine("Invalid name, try later after mountain");
+                }
+                else
+                {
+                    Console.WriteLine("Enter x coord: ");
+                    xCoord = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Enter y coord: ");
+                    yCoord = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Enter description: ");
+                    description = Console.ReadLine();
+                    Console.WriteLine("Enter speed: ");
+                    spead = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Enter river long: ");
+                    riverLong = Convert.ToInt32(Console.ReadLine());
+                    r.GetInfo(xCoord, yCoord, name, description, spead, riverLong);
+                    r.ShowInfo();
+                }
 
-            r.GetInfo(xCoord, yCoord, name, description, spead, riverLong);
-            r.ShowInfo();
 
-            Console.WriteLine("Enter mountain params: \n");
+                Console.WriteLine("\nEnter mountain params: ");
 
-            Console.WriteLine("Enter x coord: ");
-            xCoord = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter y coord: ");
-            yCoord = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter name: ");
-            name = Console.ReadLine();
-            Console.WriteLine("Enter description: ");
-            description = Console.ReadLine();
-            Console.WriteLine("Enter highest point of mountain: ");
-            highestPoint = Convert.ToInt32(Console.ReadLine());
-
-            Mountain m = new Mountain();
-            m.GetInfo(xCoord, yCoord, name, description, highestPoint);
-            m.ShowInfo();
-        }
-        catch(FormatException)
-        {
-            Console.WriteLine("You enter wrong param");
+                Console.WriteLine("Enter name: ");
+                name = Console.ReadLine();
+                flag = char.IsDigit(name[0]);
+                if (flag)
+                {
+                    Console.WriteLine("Invalid name, try later after river");
+                }
+                else if (name[0] == '+')
+                {
+                    Console.WriteLine("Invalid name, try later after river");
+                }
+                else if (name[0] == '-')
+                {
+                    Console.WriteLine("Invalid name, try later after river");
+                }
+                else
+                {
+                    Console.WriteLine("Enter x coord: ");
+                    xCoord = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Enter y coord: ");
+                    yCoord = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Enter description: ");
+                    description = Console.ReadLine();
+                    Console.WriteLine("Enter highest point of mountain: ");
+                    highestPoint = Convert.ToInt32(Console.ReadLine());
+                    m.GetInfo(xCoord, yCoord, name, description, highestPoint);
+                    m.ShowInfo();
+                }         
+                
+                Console.WriteLine("If you want close program enter 1");
+                int close = Convert.ToInt32(Console.ReadLine());
+                if(close == 1)
+                {
+                    Console.WriteLine("Program was closing");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("We continue\n");
+                }
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("You enter wrong param");
+            }
         }
 
         Console.ReadLine();
--- hw4_4/hw4_4_2/Program.cs
+++ hw4_4/hw4_4_2/Program.cs
@@ -28,7 +28,7 @@ class Mountain : GeograficObj
     public void GetInfo(int xCoord, int yCoord, string name, string description, int highestPoint)
     {
         res = "x coord: " + xCoord.ToString() + " - y cood: " + yCoord.ToString() + " - name: " +
-            name + " - description: " + description + " - highest point: " + highestPoint.ToString();
+            name + " - description: " + description + " - highest point: " + highestPoint.ToString() + "\n";
     }
     public void ShowInfo()
     {
@@ -50,48 +50,78 @@ internal class Program
         int spead;
         int riverLong;
         int highestPoint;
-        try
+        River r = new River();
+        Mountain m = new Mountain();
+        while (true)
         {
-            Console.WriteLine("Enter river params: \n");
-
-            Console.WriteLine("Enter x coord: ");
-            xCoord = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter y coord: ");
-            yCoord = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter name: ");
-            name = Console.ReadLine();
-            Console.WriteLine("Enter description: ");
-            description = Console.ReadLine();
-            Console.WriteLine("Enter speed: ");
-            spead = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter river long: ");
-            riverLong = Convert.ToInt32(Console.ReadLine());
-
-            River r = new River();
-            r.GetInfo(xCoord, yCoord, name, description, spead, riverLong);
-            r.ShowInfo();
+            try
+            {
+                Console.WriteLine("Enter river params: ");
 
 
-            Console.WriteLine("Enter mountain params: \n");
+                Console.WriteLine("Enter name: ");
+                name = Console.ReadLine();
+                bool flag = char.IsDigit(name[0]);
+                if (flag)
+                {
+                    Console.WriteLine("Invalid name, try later after mountain");
+                }
+                else
+                {
+                    Console.WriteLine("Enter x coord: ");
+                    xCoord = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Enter y coord: ");
+                    yCoord = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Enter description: ");
+                    description = Console.ReadLine();
+                    Console.WriteLine("Enter speed: ");
+                    spead = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Enter river long: ");
+                    riverLong = Convert.ToInt32(Console.ReadLine());
+                    r.GetInfo(xCoord, yCoord, name, description, spead, riverLong);
+                    r.ShowInfo();
+                }
 
-            Console.WriteLine("Enter x coord: ");
-            xCoord = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter y coord: ");
-            yCoord = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter name: ");
-            name = Console.ReadLine();
-            Console.WriteLine("Enter description: ");
-            description = Console.ReadLine();
-            Console.WriteLine("Enter highest point of mountain: ");
-            highestPoint = Convert.ToInt32(Console.ReadLine());
 
-            Mountain m = new Mountain();
-            m.GetInfo(xCoord, yCoord, name, description, highestPoint);
-            m.ShowInfo();
-        }
-        catch (FormatException)
-        {
-            Console.WriteLine("You enter wrong param");
+                Console.WriteLine("\nEnter mountain params: ");
+
+                Console.WriteLine("Enter name: ");
+                name = Console.ReadLine();
+                flag = char.IsDigit(name[0]);
+                if (flag)
+                {
+                    Console.WriteLine("Invalid name, try later after mountain");
+                }
+                else
+                {
+                    Console.WriteLine("Enter x coord: ");
+                    xCoord = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Enter y coord: ");
+                    yCoord = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Enter description: ");
+                    description = Console.ReadLine();
+                    Console.WriteLine("Enter highest point of mountain: ");
+                    highestPoint = Convert.ToInt32(Console.ReadLine());
+                    m.GetInfo(xCoord, yCoord, name, description, highestPoint);
+                    m.ShowInfo();
+                }
+
+                Console.WriteLine("If you want close program enter 1");
+                int close = Convert.ToInt32(Console.ReadLine());
+                if (close == 1)
+                {
+                    Console.WriteLine("Program was closing");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("We continue\n");
+                }
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("You enter wrong param");
+            }
         }
 
         Console.ReadLine();
